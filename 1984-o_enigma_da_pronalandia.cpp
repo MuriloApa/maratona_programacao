f2af8ee21a171565bd36bc8c0e0dc3487e415400
Murilo Aparecido <100038314+MuriloApa@users.noreply.github.com>
2024-02-03 18:29:22 -0300

    Create 1984-o_enigma_da_pronalandia.cpp

---
+++ 1984-o_enigma_da_pronalandia.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string numero;
+    long long int n0;
+    int peso = 1;
+
+    cin >> n;
+
+    while (n != 0)
+    {
+        cout << n % 10;
+        n /= 10;
+    }
+
+    cout << endl;
+
+    return 0;
+}
+
